5da614b11b9f8cbcfb9cb9b7d7c70d52b6bd2fec
Lucas Diego <luucasdiego_12@hotmail.com>
2022-01-29 00:24:21 -0400

    Componente criar produto, modelo e service

---
+++ frontend/src/app/components/product/products-create/product-create.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-products-create",
+  templateUrl: "./products-create.component.html",
+  styleUrls: ["./products-create.component.css"],
+})
+export class ProductsCreateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
