c7f5760a1e2ad0d483f4b22fad98442efa047246
Lucas Diego <luucasdiego_12@hotmail.com>
2022-02-03 21:07:45 -0400

    Tabela Produtos

--- frontend/src/app/components/products/products-read2/products-read2.component.ts
+++ frontend/src/app/components/products/products-read2/products-read2.component.ts
@@ -1,22 +1,23 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
-import { ProductsRead2DataSource, ProductsRead2Item } from './products-read2-datasource';
+import { Product } from "./../product.model";
+import { AfterViewInit, Component, ViewChild } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTable } from "@angular/material/table";
+import { ProductsRead2DataSource } from "./products-read2-datasource";
 
 @Component({
-  selector: 'app-products-read2',
-  templateUrl: './products-read2.component.html',
-  styleUrls: ['./products-read2.component.css']
+  selector: "app-products-read2",
+  templateUrl: "./products-read2.component.html",
+  styleUrls: ["./products-read2.component.css"],
 })
 export class ProductsRead2Component implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<ProductsRead2Item>;
+  @ViewChild(MatTable) table!: MatTable<Product>;
   dataSource: ProductsRead2DataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ["id", "name"];
 
   constructor() {
     this.dataSource = new ProductsRead2DataSource();
